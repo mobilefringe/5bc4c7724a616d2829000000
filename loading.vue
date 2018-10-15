18a846027f4c79bdcc905e199533d39403252f49
jamie.simpson@gmail.com @ CodeCloud Staging <jamie.simpson@gmail.com @ CodeCloud Staging>
2018-10-15 13:09:01 -0400

    Cerritos Towne Center 1.0  /loading.vue

---
+++ loading.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-if="dataLoaded" id="overlay">
+        <div class="loading-container">
+            <div class="loader"></div>
+            <p class="main_container center">{{ property.name }} is Loading...</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex"], function (Vue, Vuex) {
+        return Vue.component("loading-spinner", {
+            template: template, // the variable template will be injected,
+            data: function () {
+                return {
+                    dataLoaded: false
+                };
+            },
+            created (){
+                this.loadData().then(response => {
+                    this.dataLoaded = true;
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ])
+            },
+            methods: {
+                loadData: async function () {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "property")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+    });
+</script>
