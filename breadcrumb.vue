f31f0bf8f7e48ed0c2d8d9c86fd8862afb8e6801
jamie.simpson@gmail.com @ CodeCloud Staging <jamie.simpson@gmail.com @ CodeCloud Staging>
2018-10-15 13:48:59 -0400

    Cerritos Towne Center 1.0  /breadcrumb.vue

---
+++ breadcrumb.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <p class="page-breadcrumb">
+                <router-link to="/">Home</router-link>
+                <breadcrumbs></breadcrumbs>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+define(["Vue", "vuex", "vue-breadcrumbs"], function (Vue, Vuex, VueBreadcrumbs) {
+    Vue.use(VueBreadcrumbs);
+    return Vue.component("breadcrumb", {
+        template: template, // the variable template will be injected
+        data: function data() {
+            return {
+                breadcrumb: null,
+            }
+        },
+        computed: {
+            ...Vuex.mapGetters([
+                'property'
+            ])
+        }
+    });
+});
+</script>
